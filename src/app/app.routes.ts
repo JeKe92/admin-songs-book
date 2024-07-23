516225db93c45f1efa61042a4d8aaa85a2e3578f
Kevin Triana <jeke20@gmail.com>
2024-07-23 09:25:50 -0500

    First commit: Install - layout creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,65 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'calendar',
+        pathMatch: 'full'
+    },
+    // {
+    //     path: '',
+    //     component: DefaultLayoutComponent,
+    //     data: {
+    //       title: 'Home'
+    //     },
+    //     children: [
+    //       {
+    //         path: 'calendar',
+    //         loadChildren: () => import('./views/dashboard/routes').then((m) => m.routes)
+    //       },
+        //   {
+        //     path: 'theme',
+        //     loadChildren: () => import('./views/theme/routes').then((m) => m.routes)
+        //   },
+        //   {
+        //     path: 'base',
+        //     loadChildren: () => import('./views/base/routes').then((m) => m.routes)
+        //   },
+        //   {
+        //     path: 'buttons',
+        //     loadChildren: () => import('./views/buttons/routes').then((m) => m.routes)
+        //   },
+        //   {
+        //     path: 'forms',
+        //     loadChildren: () => import('./views/forms/routes').then((m) => m.routes)
+        //   },
+        //   {
+        //     path: 'icons',
+        //     loadChildren: () => import('./views/icons/routes').then((m) => m.routes)
+        //   },
+        //   {
+        //     path: 'notifications',
+        //     loadChildren: () => import('./views/notifications/routes').then((m) => m.routes)
+        //   },
+        //   {
+        //     path: 'widgets',
+        //     loadChildren: () => import('./views/widgets/routes').then((m) => m.routes)
+        //   },
+        //   {
+        //     path: 'charts',
+        //     loadChildren: () => import('./views/charts/routes').then((m) => m.routes)
+        //   },
+        //   {
+        //     path: 'pages',
+        //     loadChildren: () => import('./views/pages/routes').then((m) => m.routes)
+    //     //   }
+    //     ]
+    //   },
+    {
+        path: 'calendar',
+        loadComponent: () => import('./features/calendar/calendar.component').then(m => m.CalendarComponent),
+        data: {
+          title: 'Page 404'
+        }
+      },
+];
